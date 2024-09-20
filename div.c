f24fdaf8034d56bafda14f73535e5494c0d36377
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-09-20 23:29:01 +0200

    Add files via upload

--- div.c
+++ div.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+    int tal1 = atoi(argv[1]);
+    int tal2 = atoi(argv[2]);
+    if (tal2 == 0) {
+        printf("DIV ZERO NOT ALLOWED!\n");
+        return 1; 
+    }
+    double kvot = (double)tal1 / tal2;
+    printf("Kvoten är %.4f\n", kvot);
 
-  return 0;
+    return 0; 
 }
--- sub.c
+++ sub.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+    int tal1 = atoi(argv[1]);
+    int tal2 = atoi(argv[2]);
+    int skillnad = tal1 - tal2;
+    printf("Skillnaden är %d\n", skillnad);
 
-  return 0;
+    return 0; 
 }
